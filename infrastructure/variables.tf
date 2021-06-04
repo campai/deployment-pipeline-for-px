10d1f27ad8a56e3808c92e7de479b69cd5408572
Campai <the.campai@gmail.com>
2021-06-04 11:06:58 +0200

    feat: add initial, test Terraform & Kubernetes scripts

---
+++ infrastructure/variables.tf
@@ -0,0 +1,16 @@
+variable "jenkins_instance_type" {
+  description = "Instance type for Jenkins VMs"
+  type = string
+  default = "t2.micro"
+}
+
+variable "private_subnet_hosts" {
+  description = "Available private subnet's hosts"
+  type = list(string)
+  default = [
+    "192.168.1.2",
+    "192.168.1.3",
+    "192.168.1.8",
+    "192.168.1.102"
+  ]
+}
