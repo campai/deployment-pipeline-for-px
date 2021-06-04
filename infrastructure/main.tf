df9b2b7957930081a1ef1e2beb25cfb1c40d16ab
Campai <the.campai@gmail.com>
2021-06-04 11:06:58 +0200

    feat: add initial, test Terraform & Kubernetes scripts

---
+++ infrastructure/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_instance" "jenkins_machine" {
+  ami = "image code here"
+  instance_type = var.jenkins_instance_type
+
+  tags = {
+    Name = "jenkins"
+  }
+}
+
+resource "aws_vpc" "project-x-vpc" {
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    "Name" = "Project-X VPC"
+  }
+}
+
+resource "aws_subnet" "production-subnet" {
+  vpc_id = aws_vpc.project-x-vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    "Name" = "Prod"
+  }
+}
+
+resource "aws_subnet" "dev-subnet" {
+  vpc_id = aws_vpc.project-x-vpc.id
+  cidr_block = "10.0.3.0/24"
+  tags = {
+    "Name" = "Dev"
+  }
+}
