e0d81161be0f5dd086bfc6520d4ae330cf329450
Campai <the.campai@gmail.com>
2021-06-05 19:01:23 +0200

    refactor: split monolith into modules and workspaces

---
+++ infrastructure/modules/security/main.tf
@@ -0,0 +1,33 @@
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow web traffic"
+  description = "Allows HTTP and HTTPS traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "allow HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "allow HTTPS traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-web"
+  }
+}
