a1bb2a614d5ce79f95899ca64b8cc1d328625141
Campai <the.campai@gmail.com>
2021-06-05 18:13:47 +0200

    chore: mark credentials as sensitive data + gitignore added

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.44.0"
+    }
+  }
+}
+
 provider "aws" {
-  region     = var.aws-region
-  access_key = ""
-  secret_key = ""
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_vpc" "project-x-vpc" {
